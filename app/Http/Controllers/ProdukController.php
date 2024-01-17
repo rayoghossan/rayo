a161ca329e27d0210b5d6ea8a6f205b659342924
rayoghossan <rayoghossan200@gmail.com>
2024-01-17 12:35:55 +0700

    onlineshop

---
+++ app/Http/Controllers/ProdukController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Produk;
+
+class ProdukController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $rsetProduk = Produk::All();
+        return view('index', compact('rsetProduk'));
+    }
+    public function produk()
+    {
+        $rsetProduk = Produk::All();
+        return view('products', compact('rsetProduk'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $details = Produk::findOrFail($id);
+        return view('product-details', compact('details'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
